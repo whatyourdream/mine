9f8a9f98f031d8fd0070e06888c019f0ec3b3b56
Alvinditya Saputra <alvindityas@gmail.com>
2019-03-20 21:22:30 +0700

    Create Your Magic

--- app/controller/AppController.py
+++ app/controller/AppController.py
@@ -2,38 +2,65 @@ from flask import request, render_template, jsonify
 from app import app
 from app.module import AppModule
 import pandas as pd
+pd.set_option('display.max_colwidth', -1)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template("index.html")
 
+
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
-        AppModule.saveDataset(request.files['file'])
-        doneMessage = '<div class="alert alert-success"><strong>Success!</strong> Datasets is updated.</div>'
-        return render_template('upload.html', done=doneMessage)
+        if 'file' not in request.files:
+            message = """
+                <div class="alert alert-danger alert-dismissible fade show mb-0" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true">×</span>
+                </button>
+                <i class="fa fa-info mx-2"></i>
+                <strong>Failed!</strong> Should upload file first.</div>
+            """
+        else:
+            message = """
+                <div class="alert alert-success alert-dismissible fade show mb-0" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true">×</span>
+                </button>
+                <i class="fa fa-info mx-2"></i>
+                <strong>Success</strong> Datasets is updated.</div>
+            """
+            AppModule.saveDataset(request.files['file'])
+        return render_template('upload.html', message=message)
     else:
         return render_template("upload.html")
 
 
-@app.route('/api/get-result', methods=['GET'])
+@app.route('/search', methods=['GET'])
 def getResult():
-    q = None if request.args['q'] is "" else request.args['q']
-    response = []
-    dataset = AppModule.loadDataset()
-    datapreprocess = AppModule.loadDataPreprocessing()
-    scores = AppModule.search(q, datapreprocess)
-    df = pd.DataFrame(dataset['Bahan'])
-    df['Score'] = scores
-    df['Information'] = AppModule.RelevantChecker(df['Score'])
-    result = df.sort_values(by=['Score'], ascending=[False])
-    for ingredients, score, information in result.values:
-        resp = {
-            "information": information,
-            "ingredients": ingredients,
-            "score": score
-        }
-        response.append(resp)
-    return jsonify(response)
+    if 'q' in request.args:
+        if request.args['q'] != "":
+            q = request.args['q']
+            dataset = AppModule.loadDataset()
+            datapreprocess = AppModule.loadDataPreprocessing()
+            scores = AppModule.search(q, datapreprocess)
+            df = pd.DataFrame(dataset['Bahan'])
+            df['Score'] = scores
+            df['Information'] = AppModule.RelevantChecker(df['Score'])
+            result = df.sort_values(by=['Score'], ascending=[False])
+            return render_template('result.html', tables=[
+                result.to_html(classes="table mb-0 border-0 table-responsive", justify='unset', index=False).replace(
+                    'border="1"', "")])
+        else:
+            message = """
+                <div class="alert alert-danger alert-dismissible fade show mb-0" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true">×</span>
+                </button>
+                <i class="fa fa-info mx-2"></i>
+                <strong>Failed!</strong> Should contain text.</div>
+            """
+            return render_template('search.html', message=message)
+    else:
+        return render_template('search.html')
